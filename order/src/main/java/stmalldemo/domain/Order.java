0e8683d79a8545f8521a99eb052cfaa5560f26da
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2024-11-20 13:04:38 +0900

    commit

---
+++ order/src/main/java/stmalldemo/domain/Order.java
@@ -0,0 +1,50 @@
+package stmalldemo.domain;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import stmalldemo.OrderApplication;
+import stmalldemo.domain.OrderCanceled;
+import stmalldemo.domain.OrderPlaced;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String userId;
+
+    private String productName;
+
+    private String productId;
+
+    private Integer qty;
+
+    private String address;
+
+    private String status;
+
+    @PostPersist
+    public void onPostPersist() {
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+
+        OrderCanceled orderCanceled = new OrderCanceled(this);
+        orderCanceled.publishAfterCommit();
+    }
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = OrderApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+}
+//>>> DDD / Aggregate Root
